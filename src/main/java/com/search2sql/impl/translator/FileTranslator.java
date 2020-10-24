2f0011a599818d971bfca1b81f897d257420916e
fuggerjaki61 <jakob@stuhler.de>
2020-10-24 14:34:33 +0200

    Adding FileTranslator
    Started adding a new implementation that reads it data from a file to be modified more easily.

---
+++ src/main/java/com/search2sql/impl/translator/FileTranslator.java
@@ -0,0 +1,12 @@
+package com.search2sql.impl.translator;
+
+import com.search2sql.query.Query;
+import com.search2sql.translator.Translator;
+
+public class FileTranslator extends Translator {
+
+    @Override
+    public String translate(Query query) {
+        return null;
+    }
+}

8f230fad3aa0dfe8319b98a4d76a2570ebc69810
fuggerjaki61 <52215891+fuggerjaki61@users.noreply.github.com>
2021-05-05 18:38:44 +0200

    continued DateParser

--- src/main/java/com/search2sql/impl/parser/provided/DateParser.java
+++ src/main/java/com/search2sql/impl/parser/provided/DateParser.java
@@ -5,11 +5,41 @@ import com.search2sql.impl.parser.RangeParser;
 import com.search2sql.parser.SearchParser;
 import com.search2sql.query.SubQuery;
 
+import java.text.DateFormat;
+import java.util.Locale;
+
 @SearchParser("default.date")
 public class DateParser extends RangeParser {
 
+    private DateFormat dateFormat;
+
+    public DateParser() {
+        this(Locale.getDefault());
+    }
+
+    public DateParser(Locale locale) {
+        super();
+
+        this.dateFormat = DateFormat.getDateInstance(DateFormat.SHORT, locale);
+    }
+
+    public DateParser(Locale locale, String delimiter) {
+        super(delimiter);
+
+        this.dateFormat = DateFormat.getDateInstance(DateFormat.SHORT, locale);
+    }
+
+    @Override
+    public boolean isParserFor(String subQuery) {
+
+
+        return super.isParserFor(subQuery);
+    }
+
     @Override
     public SubQuery parse(String subQuery) throws InvalidSearchException {
+        //new SimpleDateFormat()
+
         return null;
     }
 }
